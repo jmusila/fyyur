ebc1784679217806503093585dcfef8daa58af44
jonathanmusila <jonathanmusila6@gmail.com>
2019-11-13 12:32:52 +0300

    connected to the database

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-
+DB_URL = os.getenv('DATABASE_URL')
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'DB_URL'
