c719b130b19f0aa0eb7b3e69e55ce5d4cfa23750
jonathanmusila <jonathanmusila6@gmail.com>
2019-11-13 12:47:05 +0300

    files rearrangement

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from backend import config
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
 moment = Moment(app)
-app.config.from_object('config')
+app.config.from_object(config)
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
